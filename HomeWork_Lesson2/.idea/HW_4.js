4a27a8da9ec2b91f4cd10a698243eea4d5de59d3
IgorTsybulin <igor.zybulin@gmail.com>
2016-09-28 13:48:45 +0300

    Deleted Idea Folder

--- HomeWork_Lesson2/.idea/HW_4.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by igortsybulin on 18.09.16.
- */
-
-var number = prompt("Choose your number, please.");
-
-modulo = (number % 13) ? alert("The remainder is " + number % 13) : alert(number + " is evenly devisible.");
--- HomeWork_Lesson2/.idea/HW_5.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by igortsybulin on 18.09.16.
- */
-
-var a = prompt("Define a-parameter");
-var b = prompt("Define b-parameter");
-
-result = (+a + +b < 4) ? alert("Мало") : alert("Много") ;
